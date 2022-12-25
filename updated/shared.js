eed972e486f9a273b03dfecf9d13ce583378c4d4
rdprassy1 <121393641+rdprassy1@users.noreply.github.com>
2022-12-25 22:47:47 +0530

    Create shared.js

---
+++ updated/shared.js
@@ -0,0 +1,25 @@
+function calculateTaxM(val1, val2) {
+    let val = val1 + val2 + 100;    
+    let data = {type: 'Response from External JS:', val};
+    sendToMobile(data);
+}
+
+function calculateTotalM(val1, val2) {
+    let val = val1 + val2 + 200;
+    let data = {type: 'Response from External JS:', val};
+    sendToMobile(data);
+}
+
+function sendToMobile(data) {
+  window.ReactNativeWebView.postMessage(JSON.stringify(data));
+}
+
+ // For Web
+
+window.calculateTax = (val1, val2) => {
+    return  val1 + val2 + 30;
+}
+
+window.calculateVals = (val1, val2) => {
+    return val1 + val2+10;
+}
