75f3796b8288f01495883bb91b8624eae5454af9
rdprassy1 <121393641+rdprassy1@users.noreply.github.com>
2023-01-05 10:55:02 +0530

    Create sharedAP.js

---
+++ ap/sharedAP.js
@@ -0,0 +1,25 @@
+function getTax(val1, val2) {
+    let val = val1 + val2 + 20;    
+    let data = {type: 'Response from External JS:', val};
+    sendToMobile(data);
+}
+
+function getToal(val1, val2) {
+    let val = val1 + val2;
+    let data = {type: 'Response from External JS:', val};
+    sendToMobile(data);
+}
+
+function sendToMobile(data) {
+  window.ReactNativeWebView.postMessage(JSON.stringify(data));
+}
+
+ // For Web
+
+window.calculateTax = (val1, val2) => {
+    return  val1 + val2 + 20;
+}
+
+window.calculateVals = (val1, val2) => {
+    return val1 + val2;
+}
