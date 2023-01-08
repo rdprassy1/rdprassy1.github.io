dc84a588824f86e92c45924d0e0b13eb924c8d76
rdprassy1 <121393641+rdprassy1@users.noreply.github.com>
2023-01-08 23:45:39 +0530

    Update sharedAP.js

--- ap/sharedAP.js
+++ ap/sharedAP.js
@@ -8,28 +8,6 @@ class Cart {
 }
 
 
-function getTax(val1, val2) {
-    let val = val1 + val2 + 20;    
-    let data = {type: 'Response from External JS:', val: val};
-    sendToMobile(data);
-}
-
-// function getToal2(val1) {
-//   let val = val1;
-//   let price = 0;
-//   if (typeof val !== 'object') {
-//     val = JSON.parse(val1);
-//   }
-//   price = parseInt(val.num1) * 100 + parseInt(val.num2) * 200;
-//   val = price ? price : 'error in external JS'
-
-//   let data = {
-//     type: 'Response from External JS:',
-//     val: price
-//   };
-//   sendToMobile(data);
-// }
-
 function getToal1(reqObj) {
 
     let tempObj = reqObj;
@@ -56,26 +34,6 @@ function getToal1(reqObj) {
 
 }
 
-function getToal(val1, val2) {
-    let val = val1 + val2;
-    let data = {type: 'Response from External JS:', val};
-    sendToMobile(data);
-}
-
-function getBasicData(val1) {
-    let data = {type: 'Response from External JS:', val: 25};
-    sendToMobile(data);
-}
-
-function getBasicData1(reqObj) {
-//     let reqObj1 = JSON.parse(reqObj);
-//     let val = reqObj1.num1 + reqObj1.num2;
-    let data = 25;
-    sendToMobile(data);
-    
-}
-
-
 
 function sendToMobile(data) {
   window.ReactNativeWebView.postMessage(JSON.stringify(data));
@@ -91,14 +49,3 @@ window.calculateVals = (val1, val2) => {
     return val1 + val2;
 }
 
-
-window.getBasicData = (reqObj) => {
-    console.log(reqObj);
-    console.log(reqObj.num1);
-    console.log(reqObj.num2);
-    console.log(typeof(reqObj));
-    let reqObj1 = JSON.parse(reqObj);
-    let val = reqObj.num1 + reqObj.num2;
-    let data = {type: 'Response from External JS:', value: val};
-    return reqObj.value;
-}
